5b57410c4d16d0ee7ee353df0a9f789205891fa5
Leonardo Castro Mestra <leoncam83@hotmail.com>
2020-08-09 19:44:45 -0500

    Initital Commit

---
+++ calculadora.go
@@ -0,0 +1,63 @@
+package mycalculator
+
+// go mod init github.com/leoncam83/mycalculator crea un archivo modulo.
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type calc struct{}
+
+func (calc) operate(entrada string, operador string) int {
+
+	entradaLimpia := strings.Split(entrada, operador)
+
+	operador1 := parsear(entradaLimpia[0])
+	operador2 := parsear(entradaLimpia[1])
+
+	var valor int = 0
+
+	switch operador {
+	case "+":
+		valor = operador1 + operador2
+	case "-":
+		valor = operador1 - operador2
+	case "*":
+		valor = operador1 * operador2
+	case "/":
+		valor = operador1 / operador2
+	default:
+		valor = 0
+	}
+
+	return valor
+}
+
+func parsear(entrada string) int {
+	opetador, _ := strconv.Atoi(entrada)
+	return opetador
+}
+
+func leerEntrada(texto string) string {
+	fmt.Println(texto)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func main() {
+
+	c := calc{}
+
+	operador := leerEntrada("Operador +,-,*,/")
+	entrada := leerEntrada("Valores: ")
+
+	valor := c.operate(entrada, operador)
+
+	fmt.Println(valor)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leoncam83/mycalculator
+
+go 1.14
